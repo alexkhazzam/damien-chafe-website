5eaea6b86be64f76d22aa06e5a4278db661c5c5e
alexkhazzam <alex722khazzam@gmail.com>
2020-12-16 22:53:09 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const Express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = Express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(Express.static(path.join(__dirname, "public")));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use("/", (req, res, next) => {
+  res.send("hi there");
+});
+
+module.exports = app;
